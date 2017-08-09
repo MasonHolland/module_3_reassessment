51bed4b6a8a72aa79fc1fecfe5ba992d2d634348
MasonHolland <masontholland@gmail.com>
2017-08-09 07:49:31 -0600

    added application.yml to gitignore. updated structure for service

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -1,9 +1,23 @@
 class ZomatoService
+  attr_reader :city
+
   def initialize(params)
     @city = params[:city]
   end
 
   def search_by_city
+    city_id = city_lookup
 
+  end
+
+  private
+
+  def city_lookup
+  end
+
+  def cuisines_search(city_id)
+  end
+
+  def restaurants_search(city_id)
   end
 end
