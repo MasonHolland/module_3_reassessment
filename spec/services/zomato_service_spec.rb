10e77ceaf726a850e138a135fe1c3357a9c92dff
MasonHolland <masontholland@gmail.com>
2017-08-09 10:39:25 -0600

    created service test for zomato call

--- spec/services/zomato_service_spec.rb
+++ spec/services/zomato_service_spec.rb
@@ -1,20 +1,50 @@
 require 'rails_helper'
 
-RSpec.service "zomato service can make api calls" do
-  context "it returns desired products" do
+describe ZomatoService do
+  attr_reader :service
+
+  before(:each) do
+    params = { "city" => "Denver" }
+    @service = ZomatoService.new(params)
+  end
+
+  describe "#cities" do
     it "returns city id when provided with city" do
-      params = { "city" => "Denver"}
-      zomser = ZomatoService.new(params).search_by_city
+      VCR.use_cassette("services/find_city_id") do
+        city_id = service.search_by_city
 
-      expect(zomser).to eq(305)
+        expect(city_id).to eq(305)
+      end
     end
+  end
 
-    xit "returns top 5 cuisines when provided with city_id" do
+  describe "#cuisines" do
+    it "returns top 5 cuisines when provided with city_id" do
+      VCR.use_cassette("services/cuisines_search") do
+        cuisines = service.search_for_cuisines
+        cuisine = cuisines.first
 
+        expect(cuisines.count).to eq(5)
+        expect(cuisine[:name]).to eq("Afghani")
+        expect(cuisine[:id]).to eq(6)
+        expect(cuisines.last[:name]).to eq("Armenian")
+        expect(cuisines.last[:id]).to eq(175)
+      end
     end
+  end
 
-    xit "returns top 5 restaurants when provided with city_id" do
+  describe "#restaurants" do
+    it "returns top 5 restaurants when provided with city_id" do
+      VCR.use_cassett("services/restaurants_search") do
+        restaurants = service.search_for_restaurants
+        restaurant = restaurants.first["restaurant"]
 
+        expect(restaurants.count).to eq(25)
+        expect(restaurant["id"]).to eq("16973776")
+        expect(restaurant["name"]).to eq("Park Burger")
+        expect(restaurant["location"]["address").to eq("1890 South Pearl Street 80210")
+        expect(restaurant["location"]["locality"]).to eq("South Pearl-Platt Park")
+      end
     end
   end
 end
